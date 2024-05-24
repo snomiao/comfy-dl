b9cde6c376f28450810042bd8e23084b265f1b69
snomiao <snomiao@gmail.com>
2024-05-24 15:30:31 +0000

    fix(main): redir
    redir

---
+++ next/app/[filename]/route.tsx
@@ -0,0 +1,13 @@
+import { NextRequest } from "next/server";
+import { getHugginfaceDownloadLink } from "../../../getHugginfaceDownloadLink";
+
+export const GET = async (req: NextRequest, { params: { filename = "" } }) => {
+  const q = req.nextUrl.pathname.slice(1);
+  const downloadLink = await getHugginfaceDownloadLink(q);
+  if (!downloadLink) return new Response("NOT FOUND", { status: 404 });
+  return new Response("Found", {
+    status: 302,
+    statusText: "Found",
+    headers: { Location: downloadLink },
+  });
+};
