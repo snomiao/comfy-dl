ade5d584d64955389dbcf8ec4d421d8ecf3dde60
snomiao <snomiao@gmail.com>
2024-05-24 15:34:14 +0000

    fix(main): deps
    deps

--- next/app/[filename]/route.tsx
+++ next/app/[filename]/route.tsx
@@ -1,8 +1,9 @@
 import { NextRequest } from "next/server";
-import { getHugginfaceDownloadLink } from "../../../getHugginfaceDownloadLink";
+import { getHugginfaceDownloadLink } from "./getHugginfaceDownloadLink";
 
 export const GET = async (req: NextRequest, { params: { filename = "" } }) => {
   const q = req.nextUrl.pathname.slice(1);
+  
   const downloadLink = await getHugginfaceDownloadLink(q);
   if (!downloadLink) return new Response("NOT FOUND", { status: 404 });
   return new Response("Found", {
